6f2a2f386e2a092a6dbe3aa763a7209606577ec3
CarlosNeto242 <carloslmn@al.insper.edu.br>
2025-05-14 22:36:30 -0300

    Ajuste das posições de "idle" do player

--- assets.py
+++ assets.py
@@ -11,14 +11,14 @@ def carrega_assets():
     assets["fonte apertar inicial"] = pygame.font.Font("Fontes/PressStart2P.ttf", 50)
     assets["fundo mapa"] = pygame.image.load("Sprites/Backgrounds/floresta.png")
     player_animacao = []
-    for i in range(1, 11):
+    for i in range(11):
         arquivo = f"Sprites/megamen/frame {i}.png"
         imagem = pygame.image.load(arquivo)
         imagem = pygame.transform.scale(imagem, (100,100))
         player_animacao.append(imagem)
     assets["animacao player"] = player_animacao
     player_animacao_atirando = []
-    for i in range(1, 11): 
+    for i in range(11): 
         arquivo1 = f"Sprites/megamen/frame atirando {i}.png"
         imagem1 = pygame.image.load(arquivo1)
         imagem1 = pygame.transform.scale(imagem1, (100,100))
